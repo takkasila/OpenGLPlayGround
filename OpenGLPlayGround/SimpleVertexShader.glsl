533f983388126f5852d6a3c3dc2b32b206b29796
Takasila Saichol <takkasila@outlook.com>
2015-08-24 00:00:11 +0700

    Loaded BMP
    Error: Texture gone blank

--- OpenGLPlayGround/SimpleVertexShader.glsl
+++ OpenGLPlayGround/SimpleVertexShader.glsl
@@ -1,15 +1,19 @@
 #version 330 core
-layout(location = 0) in vec3 vertexPosition_modelspace;
-layout(location = 1) in vec3 vertexColor;
 
+layout(location = 0) in vec3 vertexPosition_modelspace;
+layout(location = 1) in vec2 vertexUV;
+
+// will be interpolated for each fragment
+out vec2 UV;
+
+//values that stay constant for whole mesh.
 uniform mat4 MVP;
 
-out vec3 fragmentColor;
+void main()
+{
+	// Position of vertex
+	gl_Position = MVP * vec4(vertexPosition_modelspace, 1);
 
-void main(){
-
-	// Output position of the vertex, in clip space : MVP * position
-	gl_Position =  MVP * vec4(vertexPosition_modelspace,1);
-
-	fragmentColor = vertexColor;
+	//UV of the vertex
+	UV = vertexUV;
 }
--- OpenGLPlayGround/main.cpp
+++ OpenGLPlayGround/main.cpp
@@ -16,6 +16,7 @@
 using namespace glm;
 
 #include "shader.hpp"
+#include "texture.h"
 
 int main()
 {
@@ -127,71 +128,56 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
-	//Color
-	static const GLfloat g_color_buffer_data[] = {
-		0.583f, 0.771f, 0.014f,
-		0.609f, 0.115f, 0.436f,
-		0.327f, 0.483f, 0.844f,
-		0.822f, 0.569f, 0.201f,
-		0.435f, 0.602f, 0.223f,
-		0.310f, 0.747f, 0.185f,
-		0.597f, 0.770f, 0.761f,
-		0.559f, 0.436f, 0.730f,
-		0.359f, 0.583f, 0.152f,
-		0.483f, 0.596f, 0.789f,
-		0.559f, 0.861f, 0.639f,
-		0.195f, 0.548f, 0.859f,
-		0.014f, 0.184f, 0.576f,
-		0.771f, 0.328f, 0.970f,
-		0.406f, 0.615f, 0.116f,
-		0.676f, 0.977f, 0.133f,
-		0.971f, 0.572f, 0.833f,
-		0.140f, 0.616f, 0.489f,
-		0.997f, 0.513f, 0.064f,
-		0.945f, 0.719f, 0.592f,
-		0.543f, 0.021f, 0.978f,
-		0.279f, 0.317f, 0.505f,
-		0.167f, 0.620f, 0.077f,
-		0.347f, 0.857f, 0.137f,
-		0.055f, 0.953f, 0.042f,
-		0.714f, 0.505f, 0.345f,
-		0.783f, 0.290f, 0.734f,
-		0.722f, 0.645f, 0.174f,
-		0.302f, 0.455f, 0.848f,
-		0.225f, 0.587f, 0.040f,
-		0.517f, 0.713f, 0.338f,
-		0.053f, 0.959f, 0.120f,
-		0.393f, 0.621f, 0.362f,
-		0.673f, 0.211f, 0.457f,
-		0.820f, 0.883f, 0.371f,
-		0.982f, 0.099f, 0.879f
+	//Texture
+	// Two UV coordinatesfor each vertex. They were created with Blender. You'll learn shortly how to do this yourself.
+	static const GLfloat g_uv_buffer_data[] = {
+		0.000059f, 1.0f - 0.000004f,
+		0.000103f, 1.0f - 0.336048f,
+		0.335973f, 1.0f - 0.335903f,
+		1.000023f, 1.0f - 0.000013f,
+		0.667979f, 1.0f - 0.335851f,
+		0.999958f, 1.0f - 0.336064f,
+		0.667979f, 1.0f - 0.335851f,
+		0.336024f, 1.0f - 0.671877f,
+		0.667969f, 1.0f - 0.671889f,
+		1.000023f, 1.0f - 0.000013f,
+		0.668104f, 1.0f - 0.000013f,
+		0.667979f, 1.0f - 0.335851f,
+		0.000059f, 1.0f - 0.000004f,
+		0.335973f, 1.0f - 0.335903f,
+		0.336098f, 1.0f - 0.000071f,
+		0.667979f, 1.0f - 0.335851f,
+		0.335973f, 1.0f - 0.335903f,
+		0.336024f, 1.0f - 0.671877f,
+		1.000004f, 1.0f - 0.671847f,
+		0.999958f, 1.0f - 0.336064f,
+		0.667979f, 1.0f - 0.335851f,
+		0.668104f, 1.0f - 0.000013f,
+		0.335973f, 1.0f - 0.335903f,
+		0.667979f, 1.0f - 0.335851f,
+		0.335973f, 1.0f - 0.335903f,
+		0.668104f, 1.0f - 0.000013f,
+		0.336098f, 1.0f - 0.000071f,
+		0.000103f, 1.0f - 0.336048f,
+		0.000004f, 1.0f - 0.671870f,
+		0.336024f, 1.0f - 0.671877f,
+		0.000103f, 1.0f - 0.336048f,
+		0.336024f, 1.0f - 0.671877f,
+		0.335973f, 1.0f - 0.335903f,
+		0.667969f, 1.0f - 0.671889f,
+		1.000004f, 1.0f - 0.671847f,
+		0.667979f, 1.0f - 0.335851f
 	};
-	GLuint colorBuffer;
-	glGenBuffers(1, &colorBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+	GLuint cubeUVBuffer;
+	glGenBuffers(1, &cubeUVBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, cubeUVBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
+	
+	//Cube texture
+	GLuint texture = loadBMP_custom("Resources/uvtemplate.bmp");
 
-	//Triangle
-	static const GLfloat triangle_vertex_data [] = {
-		2, -1, 1
-		, 2, 1, -1
-		, 3, -1, -1
-	};
-	GLuint triangleVertexBuffer;
-	glGenBuffers(1, &triangleVertexBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, triangleVertexBuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_vertex_data), triangle_vertex_data, GL_STATIC_DRAW);
-
-	//Color
-	static const GLfloat triangle_color_data [] = {
-		1, 0, 0
-		, 0, 1, 0
-		, 0, 0, 1
-	};
-	GLuint triangleColorBuffer;
-	glGenBuffers(1, &triangleColorBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, triangleColorBuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_color_data), triangle_color_data, GL_STATIC_DRAW);
+	//Get a handle 
+	GLuint TextureID = glGetUniformLocation(programID, "myTextureSampler");
 
 	do
 	{
@@ -199,7 +185,13 @@ int main()
 		glUseProgram(programID);
 		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
-		//Cube Verticies 
+		// Bind our texture in Texture Unit 0
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		// Set sample to use this texture
+		glUniform1i(TextureID, 0);
+
+		//1st attrib: Verticies 
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 		glVertexAttribPointer(
@@ -210,13 +202,13 @@ int main()
 			, 0
 			, (void*) 0
 		);
-		
-		//Cube color
+
+		//2nd attrib: UVs
 		glEnableVertexAttribArray(1);
-		glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, cubeUVBuffer);
 		glVertexAttribPointer(
 			1
-			, 3
+			, 2
 			, GL_FLOAT
 			, GL_FALSE
 			, 0
@@ -224,30 +216,6 @@ int main()
 		);
 
 		glDrawArrays(GL_TRIANGLES, 0, 6 * 2 * 3);
-
-		//Triangle Verticies
-		glBindBuffer(GL_ARRAY_BUFFER, triangleVertexBuffer);
-		glVertexAttribPointer(
-			0
-			, 3
-			, GL_FLOAT
-			, GL_FALSE
-			, 0
-			, (void*) 0
-		);
-
-		//Triangle Color
-		glBindBuffer(GL_ARRAY_BUFFER, triangleColorBuffer);
-		glVertexAttribPointer(
-			1
-			, 3
-			, GL_FLOAT
-			, GL_FALSE
-			, 0
-			, (void*) 0
-		);
-
-		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glDisableVertexAttribArray(0);
 		glDisableVertexAttribArray(1);
@@ -260,9 +228,11 @@ int main()
 		&& glfwWindowShouldClose(window) == 0);
 
 	// Cleanup
-
 	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteBuffers(1, &cubeUVBuffer);
 	glDeleteProgram(programID);
+	glDeleteTextures(1, &TextureID);
+	glDeleteVertexArrays(1, &VertexArrayID);
 
 	// Close OpenGL windoow and terminate GLFW
 	glfwTerminate();
